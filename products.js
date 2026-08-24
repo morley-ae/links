fa4cb6b89938d3b020dd8b4db8d7343509cf975f
morley-ae <knobiwanmainn@gmail.com>
2026-08-24 16:57:21 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,106 @@
+/* Permanent product catalog. Edit with admin.html, then commit this file to GitHub. */
+window.SCENEPACK_DOWNLOAD_PASSWORD = "ENDLESSAUDIOS";
+window.PRODUCT_CATEGORIES = {
+    "preset": [
+        "Packs",
+        "CCs",
+        "Outros"
+    ],
+    "scenepack": {
+        "movieShow": [
+            "The 100",
+            "Hunger Games",
+            "The Rookie"
+        ],
+        "characterActor": [
+            "Bellamy Blake",
+            "Clarke Griffin",
+            "Octavie Blake",
+            "John Murphy"
+        ]
+    }
+};
+window.PRODUCTS_DATA = [
+    {
+        "id": 1787579517348,
+        "title": "CC 1",
+        "desc": "Description",
+        "price": "4,99€",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20all%20scenes.jpg",
+        "media": [
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20all%20scenes.jpg"
+            }
+        ],
+        "categories": [],
+        "presetCategories": [
+            "CCs"
+        ],
+        "movieShows": [],
+        "characters": [],
+        "previewVideos": [
+            "https://streamable.com/x2cwpz",
+            "https://streamable.com/w5d06v",
+            "https://streamable.com/w5d06v"
+        ],
+        "details": "details here\ndeatai\ndadkjadjk\nadada\nadjaodoa\nadoado\nadojaodj\nadojaod\nadojao\najodjaojdoa\njoadaoj",
+        "type": "preset",
+        "payhip": "https://payhip.com"
+    },
+    {
+        "id": 1787559098245,
+        "title": "SCENEPACK 1",
+        "desc": "Scenepack 1",
+        "price": "FREE",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg",
+        "media": [
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg"
+            }
+        ],
+        "categories": [],
+        "presetCategories": [],
+        "movieShows": [
+            "The 100"
+        ],
+        "characters": [
+            "Bellamy Blake",
+            "Clarke Griffin",
+            "Octavie Blake"
+        ],
+        "details": "more details",
+        "type": "scenepack",
+        "payhip": "https://payhip.com"
+    },
+    {
+        "id": 1787559030375,
+        "title": "PACK V1",
+        "desc": "Pack V1",
+        "price": "10,99€",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg",
+        "media": [
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg"
+            },
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20all%20scenes.jpg"
+            }
+        ],
+        "categories": [],
+        "presetCategories": [
+            "Packs"
+        ],
+        "movieShows": [],
+        "characters": [],
+        "previewVideos": [
+            "https://streamable.com/x2cwpz"
+        ],
+        "details": "details here",
+        "type": "preset",
+        "payhip": "https://payhip.com"
+    }
+];
