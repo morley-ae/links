89753854595c27ebf1669071bb250beef295226f
knobi <knobiwan13@gmail.com>
2026-08-25 20:00:12 +0200

    added scps

--- products.js
+++ products.js
@@ -16,130 +16,292 @@ window.PRODUCT_CATEGORIES = {
             "Bellamy Blake",
             "Clarke Griffin",
             "Octavie Blake",
-            "John Murphy"
+            "John Murphy",
+            "Bob Morley"
         ]
     }
 };
 window.PRODUCTS_DATA = [
     {
-        "id": 1787587276330,
-        "title": "TEST",
-        "desc": "test1",
-        "price": "10€",
-        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg",
+        "id": 1787680489101,
+        "title": "HUNGER GAMES SOTR - TRAILER & TEASER",
+        "desc": "TRAILER & TEASER & ISOLATED VOCALS",
+        "price": "FREE",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/hunger%20games/SOTR%20Trailer%20+%20Teaser%20cover.png",
         "media": [
             {
                 "type": "image",
-                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg"
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/hunger%20games/SOTR%20Trailer%20+%20Teaser%20cover.png"
             }
         ],
+        "isColoring": false,
+        "ccPreview": null,
         "categories": [],
-        "presetCategories": [
-            "Packs"
+        "presetCategories": [],
+        "movieShows": [
+            "Hunger Games"
         ],
-        "movieShows": [],
         "characters": [],
-        "previewVideos": [
-            "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg"
-        ],
-        "details": "",
-        "type": "preset",
-        "payhip": "Vt5VC"
-    },
-    {
-        "id": 1787579517348,
-        "title": "CC 1",
-        "desc": "Description",
-        "price": "4,99€",
-        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20all%20scenes.jpg",
-        "media": [
-            {
-                "type": "image",
-                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20all%20scenes.jpg"
-            }
-        ],
-        "categories": [],
-        "presetCategories": [
-            "CCs"
-        ],
-        "movieShows": [],
-        "characters": [],
-        "previewVideos": [
-            "https://streamable.com/x2cwpz",
-            "https://streamable.com/w5d06v",
-            "https://streamable.com/w5d06v"
-        ],
-        "details": "details here\ndeatai\ndadkjadjk\nadada\nadjaodoa\nadoado\nadojaodj\nadojaod\nadojao\najodjaojdoa\njoadaoj",
-        "type": "preset",
+        "previewVideos": [],
+        "details": "Scenes: Trailer + Teaser + Isolated Vocals\nMade by: morley.ae\nQuality: 1080p\nDuration: 4 min\nSize: 84 mb\n\nPlease give credits if you use this!",
+        "type": "scenepack",
         "payhip": "05SvN"
     },
     {
-        "id": 1787559098245,
-        "title": "SCENEPACK 1",
-        "desc": "Scenepack 1",
+        "id": 1787680178117,
+        "title": "BELLAMY BLAKE - SEASON 1",
+        "desc": "ALL SCENES",
         "price": "FREE",
-        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/bellamy/bellamy%20s1%20best%20scenes.jpg",
         "media": [
             {
                 "type": "image",
-                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg"
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/bellamy/bellamy%20s1%20best%20scenes.jpg"
             }
         ],
+        "isColoring": false,
+        "ccPreview": null,
         "categories": [],
         "presetCategories": [],
         "movieShows": [
             "The 100"
         ],
         "characters": [
-            "Bellamy Blake",
-            "Clarke Griffin",
-            "Octavie Blake"
+            "Bellamy Blake"
         ],
-        "details": "more details",
+        "previewVideos": [],
+        "details": "Season: 1\nScenes: Best Scenes\nMade by: morley.ae\nQuality: 1080p\nDuration: 69 min\nSize: 4,74 gb\n\nPlease give credits if you use this!",
         "type": "scenepack",
-        "payhip": "05SvN"
+        "payhip": "g6143"
     },
     {
-        "id": 1787559030375,
-        "title": "PACK V1",
-        "desc": "Pack V1",
-        "price": "10,99€",
-        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg",
+        "id": 1787679963456,
+        "title": "BELLAMY BLAKE - SEASON 1",
+        "desc": "ALL SCENES",
+        "price": "FREE",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/bellamy/bellamy%20s1%20all%20scenes.jpg",
         "media": [
             {
                 "type": "image",
-                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20best%20scenes.jpg"
-            },
-            {
-                "type": "image",
-                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/bellamy%20s1%20all%20scenes.jpg"
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/bellamy/bellamy%20s1%20all%20scenes.jpg"
             }
         ],
+        "isColoring": false,
+        "ccPreview": null,
         "categories": [],
-        "presetCategories": [
-            "Packs"
+        "presetCategories": [],
+        "movieShows": [
+            "The 100"
         ],
-        "movieShows": [],
-        "characters": [],
-        "previewVideos": [
-            "https://streamable.com/x2cwpz"
+        "characters": [
+            "Bellamy Blake"
         ],
-        "details": "details here",
-        "type": "preset",
+        "previewVideos": [],
+        "details": "Season: 1\nScenes: All Scenes\nMade by: morley.ae\nQuality: 1080p\nDuration: 117 min\nSize: 9,15 gb\n\nPlease give credits if you use this!",
+        "type": "scenepack",
+        "payhip": "REIUS"
+    },
+    {
+        "id": 1787679733537,
+        "title": "BELLAMY BLAKE - SEASON 1",
+        "desc": "SAD SCENES",
+        "price": "FREE",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/bellamy/bellamy%20s1%20sad%20scenes.jpg",
+        "media": [
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/bellamy/bellamy%20s1%20sad%20scenes.jpg"
+            }
+        ],
+        "isColoring": false,
+        "ccPreview": null,
+        "categories": [],
+        "presetCategories": [],
+        "movieShows": [
+            "The 100"
+        ],
+        "characters": [
+            "Bellamy Blake"
+        ],
+        "previewVideos": [],
+        "details": "Season: 1\nScenes: Sad Scenes\nMade by: morley.ae\nQuality: 1080p\nDuration: 21 min\nSize: 1,62 gb\n\nPlease give credits if you use this!",
+        "type": "scenepack",
+        "payhip": "RsVnB"
+    },
+    {
+        "id": 1787679554980,
+        "title": "BELLAMY BLAKE - SEASON 1",
+        "desc": "SOFT SCENES",
+        "price": "FREE",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/bellamy/bellamy%20s1%20soft%20scenes.jpg",
+        "media": [
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/bellamy/bellamy%20s1%20soft%20scenes.jpg"
+            }
+        ],
+        "isColoring": false,
+        "ccPreview": null,
+        "categories": [],
+        "presetCategories": [],
+        "movieShows": [
+            "The 100"
+        ],
+        "characters": [
+            "Bellamy Blake"
+        ],
+        "previewVideos": [],
+        "details": "Season: 1\nScenes: Soft Scenes\nMade by: morley.ae\nQuality: 1080p\nDuration: 14 min\nSize: 1,09 gb\n\nPlease give credits if you use this!",
+        "type": "scenepack",
+        "payhip": "zebit"
+    },
+    {
+        "id": 1787679353182,
+        "title": "BELLAMY BLAKE - SEASON 1",
+        "desc": "SLICKED BACK HAIR SCENES",
+        "price": "FREE",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/bellamy/bellamy%20s1%20slicked%20back%20hair%20scenes.jpg",
+        "media": [
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/bellamy/bellamy%20s1%20slicked%20back%20hair%20scenes.jpg"
+            }
+        ],
+        "isColoring": false,
+        "ccPreview": null,
+        "categories": [],
+        "presetCategories": [],
+        "movieShows": [
+            "The 100"
+        ],
+        "characters": [
+            "Bellamy Blake"
+        ],
+        "previewVideos": [],
+        "details": "Season: 1\nScenes: Slicked Back Hair Scenes\nMade by: morley.ae\nQuality: 1080p\nDuration: 16 min\nSize: 1,25 gb\n\nPlease give credits if you use this!",
+        "type": "scenepack",
+        "payhip": "QhigI"
+    },
+    {
+        "id": 1787678968449,
+        "title": "CLARKE GRIFFIN - SEASON 1",
+        "desc": "BEST SCENES",
+        "price": "FREE",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/clarke/clarke%20s1%20best%20scenes.jpg",
+        "media": [
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/clarke/clarke%20s1%20best%20scenes.jpg"
+            }
+        ],
+        "isColoring": false,
+        "ccPreview": null,
+        "categories": [],
+        "presetCategories": [],
+        "movieShows": [
+            "The 100"
+        ],
+        "characters": [
+            "Clarke Griffin"
+        ],
+        "previewVideos": [],
+        "details": "Season: 1\nScenes: Best Scenes\nMade by: morley.ae\nQuality: 1080p\nDuration: 79 min\nSize: 6,26 gb\n\nPlease give credits if you use this!",
+        "type": "scenepack",
         "payhip": "Vt5WC"
+    },
+    {
+        "id": 1787678833535,
+        "title": "CLARKE GRIFFIN - SEASON 1",
+        "desc": "ALL SCENES",
+        "price": "FREE",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/clarke/clarke%20s1%20all%20scenes.jpg",
+        "media": [
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/clarke/clarke%20s1%20all%20scenes.jpg"
+            }
+        ],
+        "isColoring": false,
+        "ccPreview": null,
+        "categories": [],
+        "presetCategories": [],
+        "movieShows": [
+            "The 100"
+        ],
+        "characters": [
+            "Clarke Griffin"
+        ],
+        "previewVideos": [],
+        "details": "Season: 1\nScenes: All Scenes\nMade by: morley.ae\nQuality: 1080p\nDuration: 177 min\nSize: 13,4 gb\n\nPlease give credits if you use this!",
+        "type": "scenepack",
+        "payhip": "ANXMu"
+    },
+    {
+        "id": 1787678716989,
+        "title": "OCTAVIA BLAKE - SEASON 1",
+        "desc": "ALL SCENES",
+        "price": "FREE",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/octavia/octavia%20s1%20all%20scenes.jpg",
+        "media": [
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/octavia/octavia%20s1%20all%20scenes.jpg"
+            }
+        ],
+        "isColoring": false,
+        "ccPreview": null,
+        "categories": [],
+        "presetCategories": [],
+        "movieShows": [
+            "The 100"
+        ],
+        "characters": [
+            "Octavie Blake"
+        ],
+        "previewVideos": [],
+        "details": "Season: 1\nScenes: All Scenes\nMade by: morley.ae\nQuality: 1080p\nDuration: 83 min\nSize: 6,37 gb\n\nPlease give credits if you use this!",
+        "type": "scenepack",
+        "payhip": "ai4j2"
+    },
+    {
+        "id": 1787677924984,
+        "title": "JOHN MURPHY - SEASON 1",
+        "desc": "ALL SCENES",
+        "price": "FREE",
+        "image": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/murphy/murphy%20s1%20all%20scenes.jpg",
+        "media": [
+            {
+                "type": "image",
+                "src": "https://knobiwan4-prog.github.io/morley.ae/previews/scenepacks/murphy/murphy%20s1%20all%20scenes.jpg"
+            }
+        ],
+        "isColoring": false,
+        "ccPreview": null,
+        "categories": [],
+        "presetCategories": [],
+        "movieShows": [
+            "The 100"
+        ],
+        "characters": [
+            "John Murphy"
+        ],
+        "previewVideos": [],
+        "details": "Season: 1\nScenes: All Scenes\nMade by: morley.ae\nQuality: 1080p\nDuration: 35 min\nSize: 2,17 gb\n\nPlease give credits if you use this!",
+        "type": "scenepack",
+        "payhip": "97QaS"
     }
 ];
 window.SALES_DATA = [
     {
         "id": "sale-1787588404317",
         "name": "NEW TEST SALE",
-        "code": "TEST123",
         "discountType": "percent",
         "discountValue": 25,
-        "startsAt": "2026-08-24T16:27:00.000Z",
-        "endsAt": "2026-08-24T16:29:00.000Z",
+        "startsAt": "2026-08-25T16:30:00.000Z",
+        "endsAt": "2026-08-25T16:31:00.000Z",
         "productIds": [
-            1787587276330
+            1787587276330,
+            1787559030375
         ]
     }
 ];
